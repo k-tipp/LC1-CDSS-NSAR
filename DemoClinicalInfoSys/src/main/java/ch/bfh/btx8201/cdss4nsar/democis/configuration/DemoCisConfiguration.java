e99fbc4724ac427e3d53c8c4373b2c06d4066339
Kevin Tippenhauer <kevin.tippenhauer@bfh.ch>
2016-06-14 14:57:55 +0200

    changed create-drop of db back to update and removed unused dependencies from validators

--- DemoClinicalInfoSys/src/main/java/ch/bfh/btx8201/cdss4nsar/democis/configuration/DemoCisConfiguration.java
+++ DemoClinicalInfoSys/src/main/java/ch/bfh/btx8201/cdss4nsar/democis/configuration/DemoCisConfiguration.java
@@ -155,7 +155,7 @@ public class DemoCisConfiguration {
 	@Bean
 	public Properties getAdditionalProperties() {
 		Properties properties = new Properties();
-		properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");// create-drop
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");// create-drop
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
 		properties.setProperty("hibernate.show_sql", "false");
 		return properties;
